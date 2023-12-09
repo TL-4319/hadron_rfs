bb26e0109fd71f44b6ff7efe962d4e8ff2b94acc
Tuan Luong <tdluong@crimson.ua.edu>
2023-12-09 13:04:39 -0600

    tunned rfs tracker with IR measurement

--- real_time_track_from_ir.py
+++ real_time_track_from_ir.py
@@ -13,15 +13,16 @@ from serums.models import GaussianScaleMixture
 from serums.enums import GSMTypes
 import warnings
 warnings.filterwarnings("ignore")
+from scipy.io import savemat
 
 
 def _state_mat_fun(t, dt):
     return np.array(
         [
-            [1.0, 0.0, 0.0, dt, 0.0],
-            [0.0, 1.0, 0.0, 0.0, dt],
-            [0.0, 0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 0.0, 1.0],
+            [1.0, 0.0, dt, 0.0],
+            [0.0, 1.0, 0.0, dt],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
         ]
     )
 
@@ -40,7 +41,7 @@ def set_inner_filter(filt_name, p_noise_pos, p_noise_vel, dt=None):
     if filt_name.lower() == 'kalman_filter':
         filt = gfilts.KalmanFilter()
         # filt.meas_noise = np.diag([0.0118, 0.0084])
-        filt.meas_noise = np.diag([50, 50])
+        filt.meas_noise = np.diag([1, 1])
     elif filt_name.lower() == 'students_filter':
         filt = gfilts.StudentsTFilter()
         filt.proc_noise_dof = 33
@@ -130,8 +131,8 @@ def init_rfs_filter(f_type, if_type, dt=None):
     # Set birth area at the lef and right edge of the image
     mu = [np.array([640, 200, 0, 0]).reshape((-1, 1)),
           np.array([0, 200, 0, 0]).reshape((-1, 1))]
-    cov = [np.diag(np.array([10, 200, 10, 10])) ** 2,
-           np.diag(np.array([10, 200, 10, 10])) ** 2]
+    cov = [np.diag(np.array([100, 300, 40, 40])) ** 2,
+           np.diag(np.array([100, 300, 40, 40])) ** 2]
 
     if if_type.lower() == 'kalman_filter' or if_type.lower() == 'gaussian_scale_mixture':
         gm0 = smodels.GaussianMixture(means=mu, covariances=cov, weights=[1 / len(mu) for m in mu])
@@ -148,8 +149,8 @@ def init_rfs_filter(f_type, if_type, dt=None):
     b_model = [
         (gm0, 0.1),
     ]
-    p_noise_pos = 20
-    p_noise_vel = 100
+    p_noise_pos = 3
+    p_noise_vel = 5
     m_noise = 0.2
 
     filt = set_inner_filter(if_type, p_noise_pos, p_noise_vel, dt=dt)
@@ -161,8 +162,9 @@ def init_rfs_filter(f_type, if_type, dt=None):
         "birth_terms": b_model,
         #"clutter_den": 1/(6.5*3*np.sqrt(2)/2*3/3),
         #"clutter_rate": 0.01,
-        "clutter_den": 10,
-        "clutter_rate": 10,
+        "clutter_den": 1/327680,
+        #"clutter_den": 1/1000,
+        "clutter_rate": 100,
     }
     glmb_args = {
         "req_births": len(b_model) + 1,
@@ -223,11 +225,8 @@ if __name__ == "__main__":
 
     elapsed_time = 0
 
-    fig = plt.figure()
-    fig.add_subplot(1, 1, 1)
-    plt.pause(0.001)
-    plt.xlim(0, 640)
-    plt.ylim(0, 512)
+    #fig = plt.figure()
+    #fig.add_subplot(1, 1, 1)
 
     print("Starting loop... ({} seconds)".format(args.duration))
     # extract_kwargs = {"update": True, "calc_states": True}
@@ -239,7 +238,7 @@ if __name__ == "__main__":
     last_meas_time = timer() - des_meas_rate
     min_rate = min(des_pred_rate, des_meas_rate)
     print_time = 0
-    start_time = timer()
+
     elapsed_time_vec = []
     real_time_vec = []
 
@@ -251,6 +250,7 @@ if __name__ == "__main__":
     x_list = []
     y_list = []
     time_of_interest = 0
+    start_time = timer()
     while elapsed_time < args.duration:
         loop_start_time = timer()
         needs_extract = False
@@ -287,8 +287,8 @@ if __name__ == "__main__":
             time_step = timer() - start_time
             #print (len(meas))
             rfs_filt.correct(time_step, meas)
-            print('correct called with meas: ', meas)
-            print('num hyps: ', len(rfs_filt._hypotheses))
+            #print('correct called with meas: ', meas)
+            #print('num hyps: ', len(rfs_filt._hypotheses))
             last_meas_time = timer()
             needs_extract = True
             #time_list.append(elapsed_time)
@@ -308,34 +308,49 @@ if __name__ == "__main__":
                 )
 
         if needs_extract:
+            extract_kwargs = {"update": True, "calc_states": False}
             rfs_filt.cleanup(extract_kwargs=extract_kwargs)
             elapsed_time_vec.append(timer() - start_time)
             real_time_vec.append(time_of_interest)
-            rfs_filt.plot_states_labels([0, 1], meas_inds=[0, 1], f_hndl=fig)
-            plt.pause(0.001)
+            #extract_kwargs = {"update": False, "calc_states": True}
+            #rfs_filt.extract_states(**extract_kwargs)
+            #rfs_filt.plot_states_labels([0, 1], meas_inds=[0, 1], f_hndl=fig)
+            #plt.pause(0.001)
 
         time_counter = time_counter + 1
 
         elapsed_time = timer() - start_time
         #elapsed_time += (timer() - loop_start_time)
-        if elapsed_time - print_time >= 10:
+        if elapsed_time - print_time >= 2:
             print("\tElapsed Time: {:.2f}".format(elapsed_time))
             print_time = elapsed_time
+
+
     print("Elapsed Time: {:.2f}".format(elapsed_time))
-    extract_kwargs = {"update": True, "calc_states": True}
+    extract_kwargs = {"update": False, "calc_states": True}
     rfs_filt.extract_states(**extract_kwargs)
 
 
     filt_state = rfs_filt.save_filter_state()
-    saved_outputs = {'filt_state': filt_state,
-                     'cls_type'  : type(rfs_filt).__name__,
-                     'time' : elapsed_time_vec,
-                     'real_time' : real_time_vec
-    }
-    pickle.dump(saved_outputs, open(args.out_name, "wb"))
-
+    data = {'rel_time': elapsed_time_vec,
+            'states': filt_state['_states'],
+            'covs': filt_state['_covs'],
+            'labels': filt_state['_labels'],
+            'real_time': real_time_vec}
+    savemat(
+        "/home/lagerprocessor/Projects/hadron_rfs/ir_gm_jglmb_2hz_1701966775_029.mat",
+        data)
     rfs_filt.plot_states_labels([0, 1], meas_inds=[0, 1])
     rfs_filt.plot_card_history(time_units='s',time=elapsed_time_vec)
+
+    data = {'rel_time': elapsed_time_vec,
+            'states': filt_state['_states'],
+            'covs': filt_state['_covs'],
+            'labels': filt_state['_labels'],
+            'real_time': real_time_vec}
+    savemat(
+        "ir_gm_jglmb_2hz_170196810_153.mat",
+        data)
 
     #time_list = []
     #x_list = []
@@ -354,9 +369,12 @@ if __name__ == "__main__":
     plt.plot(time_list, x_list, 'ko')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Meas x pos")
+    plt.ylim(0, 640)
     plt.subplot(2,1,2)
     plt.plot(time_list, y_list, 'ko')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Meas y pos")
-
+    plt.ylim(0, 512)
     plt.show()
+
+
